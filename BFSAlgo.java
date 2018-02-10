9c88168d38648aa9a47e34b208bcb589df96d0f6
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-02-10 08:14:13 +0800

    Explored list not functioning as intended, run time without is still faster

--- BFSAlgo.java
+++ BFSAlgo.java
@@ -25,10 +25,25 @@ public class BFSAlgo {
       }
       else {
         for (int i=0; i!=list.size(); i++){
+          System.out.println("======================================"+list.size());
           State newState = currentState.result(currentState, list.get(i));
-          if(exploredList.contains(newState) == false){
+          String [][] board = newState.getState();
+          if(exploredList.size() == 0){
             exploredList.add(newState);
-            frontier.add(newState);
+          }
+          for(int j = 0; j!=exploredList.size(); j++){
+            System.out.println("======================================"+exploredList.size());
+            if(board.toString().equals(exploredList.get(j).getState().toString())){
+              System.out.println("ya it in list");
+              System.out.println(j);
+            }
+            else{
+              // System.out.println("ya it NOT in list");
+              System.out.println(j);
+              exploredList.add(newState);
+              frontier.add(newState);
+            }
+
           }
         }
       }
